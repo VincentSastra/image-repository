38de7b52e33cdeb9a2533836e488d6a714280b3d
VincentSastra <vincent.sastra@gmail.com>
2021-09-12 20:40:01 -0700

    Create a simple get all images API Call

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,9 +1,32 @@
 <script>
-	let name = "world";
+	const apiEndpoint = "https://jfu7to45ji.execute-api.us-east-2.amazonaws.com/production"
+
+	// To do create a user account
+	const userName = "root"
+
+	let imageKeyList = []
+
+	const xmlParser = new DOMParser()
+
+	const getImages = async () => {
+		// Request a list of all images from the user's folder
+		const res = await fetch(`${apiEndpoint}/list/${userName}`)
+		const body = await res.text()
+
+		// Parse the xml folder returned by API
+		imageKeyList = [...xmlParser.parseFromString(body, "text/xml")
+			.getElementsByTagName("Contents")]
+			.map(contentXML => contentXML.getElementsByTagName("Key")[0].innerHTML)
+			// Only match the keys and not folder names
+			.filter(key => key.match(/.+\/.+/))
+	}
 </script>
 
 <div>
-	<button>Get Image</button>
+	{#each imageKeyList as imageKey}
+		<img src={`${apiEndpoint}/get/${imageKey}`} alt={imageKey} />
+	{/each}
+	<button on:click={getImages}>Get Image</button>
 	<button>Upload Image</button>
 </div>
 
